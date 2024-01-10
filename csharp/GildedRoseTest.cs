f5b6d6fa505b89962b8bd3811c6424805c27591c
Luke Narramore <luke.narramore@gmail.com>
2024-01-10 10:40:35 +0200

    Fix StandItem Test

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,12 +7,14 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void StandardItem_UpdateQuality()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Item Foo", SellIn = 10, Quality = 8 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.That("Item Foo", Is.EqualTo(Items[0].Name));
+            Assert.That(9, Is.EqualTo(Items[0].SellIn)); 
+            Assert.That(7, Is.EqualTo(Items[0].Quality));
         }
     }
 }
